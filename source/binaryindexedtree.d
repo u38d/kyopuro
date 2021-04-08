26cd6ace907edbb4aad53c3c6336e85969773168
Yusuke Muto <x85kruz@gmail.com>
2021-04-08 17:11:13 +0900

    Fix index番目の要素の計算 in BinaryIndexedTree.set

--- source/binaryindexedtree.d
+++ source/binaryindexedtree.d
@@ -35,7 +35,7 @@ struct BinaryIndexedTree(T, alias F, alias RevF, T E) {
 	}
 
     void set(size_t index, T value) {
-        auto x = get(index) - get(index - 1);
+        auto x = RevF(get(index), get(index - 1));
         x = RevF(value, x);
         add(index, x);
     }
@@ -46,21 +46,41 @@ unittest {
     import std.algorithm;
     import std.stdio;
 
-	// add
-	auto a = [1, 9, 8, 4, 3, 2, 5];
-	auto tree = BinaryIndexedTree!(int, (int a, int b) => a + b, (int a, int b) => a - b, 0)(a);
+	{
+		// add
+		auto a = [1, 9, 8, 4, 3, 2, 5];
+		auto tree = BinaryIndexedTree!(int, (int a, int b) => a + b, (int a, int b) => a - b, 0)(a);
 
-	auto b = [8, 4, 3, 0, 6, 3, 0];
-	foreach (i;0..min(a.length, b.length)) {
-		if (b[i] != 0) {
-			a[i] = b[i];
-			tree.set(i, a[i]);
+		auto b = [8, 4, 3, 0, 6, 3, 0];
+		foreach (i;0..min(a.length, b.length)) {
+			if (b[i] != 0) {
+				a[i] = b[i];
+				tree.set(i, a[i]);
+			}
+		}
+
+
+		foreach (i;0..a.length - 1) {
+	        // writeln(a[0..i + 1].sum, " : ", tree.get(i));
+			assert(a[0..i + 1].sum == tree.get(i));
 		}
 	}
+	{
+		// xor
+		auto a = [1, 9, 8, 4, 3, 2, 5];
+		auto tree = BinaryIndexedTree!(int, (int a, int b) => a ^ b, (int a, int b) => a ^ b, 0)(a);
+
+		auto b = [8, 4, 3, 0, 6, 3, 0];
+		foreach (i;0..min(a.length, b.length)) {
+			if (b[i] != 0) {
+				a[i] = b[i];
+				tree.set(i, a[i]);
+			}
+		}
 
 
-	foreach (i;0..a.length - 1) {
-        // writeln(a[0..i + 1].sum, " : ", tree.get(i));
-		assert(a[0..i + 1].sum == tree.get(i));
+		foreach (i;0..a.length - 1) {
+			assert(a[0..i + 1].reduce!"a ^ b" == tree.get(i));
+		}
 	}
 }
