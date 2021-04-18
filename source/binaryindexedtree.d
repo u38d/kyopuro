a809021201fbdeaedf12751ac49f25f34e92b55d
Yusuke Muto <x85kruz@gmail.com>
2021-04-18 12:49:41 +0900

    binaryindexedtree.d lowerBoundとそのunittestを追加。

--- source/binaryindexedtree.d
+++ source/binaryindexedtree.d
@@ -1,6 +1,7 @@
 
 struct BinaryIndexedTree(T, alias F, alias RevF, T E) {
-	T[] nodeList;
+	T[] nodeList; // ノードを格納
+	size_t length_; // 元配列の長さ
 
 	this(const(T)[] a) {
 		size_t len = 1;
@@ -10,6 +11,8 @@ struct BinaryIndexedTree(T, alias F, alias RevF, T E) {
 		nodeList = new T[len]; // 2^nの長さで確保
 		nodeList[0..a.length] = a[];
 		nodeList[a.length..$] = E;
+
+		length_ = a.length;
 
 		for (size_t i = 2;i <= nodeList.length;i *= 2) {
 			for (size_t j = i - 1;j < nodeList.length;j += i) {
@@ -48,6 +51,36 @@ struct BinaryIndexedTree(T, alias F, alias RevF, T E) {
 		auto d = RevF(value, x);
 		add(index, d);
 	}
+
+/* 二分探索。非負数の足し算のみ。
+ * get0to(lowerBound(value)) < valueとなるような関数
+ */
+	size_t lowerBound(T value) { 
+		assert(F(1, 1) == 2);
+
+		if (value <= 0) {
+			return 0;
+		}
+
+		if (nodeList.length == 0) {
+			return 0;
+		}
+
+		size_t offset = 0;
+
+		for (size_t i = nodeList.length;i > 0;i /= 2) {
+			if (offset + i - 1 < nodeList.length && nodeList[offset + i - 1] < value) {
+				value = RevF(value, nodeList[offset + i - 1]);
+				offset += i;
+			}
+		}
+
+		if (offset > length_) { // 元の配列の長さまで
+			return length_;
+		}
+
+		return offset;
+	}
 }
 
 
@@ -74,6 +107,15 @@ unittest {
 
 		foreach (i;1..a.length) {
 			assert(a[0..i].sum == tree.get(0, i));
+		}
+
+		auto c = [0, 1, 5, 10, 19, 40];
+		foreach (e;c) {
+			if (e <= 0) {
+				assert(tree.lowerBound(e) == 0);
+			} else {
+				assert(tree.get0to(tree.lowerBound(e)) < e);
+			}
 		}
 	}
 	{
